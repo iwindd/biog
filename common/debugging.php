c11f1e4d5982d4b634587a26e22a67768f506f3c
Achirawit Kaewkhong <freewinendlife@gmail.com>
2026-02-25 17:10:51 +0700

    Add TODO and dd debugging helper
    Add a project TODO file (TODO) containing task lists and progress notes (Thai). Add common/debugging.php which defines a dd() helper that uses yii\helpers\VarDumper to pretty-print data inside a styled <pre> and terminates execution for quick debugging.

---
+++ common/debugging.php
@@ -0,0 +1,12 @@
+<?php
+
+use yii\helpers\VarDumper;
+
+if (!function_exists('dd')) {
+    function dd($data){
+        echo '<pre style="color: #fff; background-color: #000;">';
+        echo VarDumper::dumpAsString($data, 10, true);
+        echo '</pre>';
+        die();
+    }
+}
